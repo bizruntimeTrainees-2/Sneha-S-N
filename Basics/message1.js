b8547432d8c3e3d24fab4e7a77041bb717c784c9
Sneha-S-N <snehasn028@gmail.com>
2020-01-14 19:53:53 +0530

    Java script basic

---
+++ Basics/message1.js
@@ -0,0 +1,6 @@
+
+function abc()
+{
+	var x=10;
+	alert("displayed value is " + x);
+}
---
+++ Basics/message2.js
@@ -0,0 +1,9 @@
+var data = 200;
+function a()
+{
+	alert("The a value is = " + data);
+}
+function b()
+{
+	document.writeln(" The b value is = " + data);
+}
