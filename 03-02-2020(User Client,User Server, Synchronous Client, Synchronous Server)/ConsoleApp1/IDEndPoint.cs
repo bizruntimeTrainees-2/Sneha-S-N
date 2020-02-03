1693b3bd27a5913394639952a6fdc008cbb49998
shraddhaprs <snehasn028@gmail.com>
2020-02-03 20:05:39 +0530

    C#

---
+++ 03-02-2020(User Client,User Server, Synchronous Client, Synchronous Server)/ConsoleApp1/IDEndPoint.cs
@@ -0,0 +1,16 @@
+﻿using System.Net;
+
+namespace ConsoleApp1
+{
+    internal class IDEndPoint : EndPoint
+    {
+        private int v1;
+        private int v2;
+
+        public IDEndPoint(int v1, int v2)
+        {
+            this.v1 = v1;
+            this.v2 = v2;
+        }
+    }
+}
